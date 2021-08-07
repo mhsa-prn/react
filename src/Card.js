103b02860ad5dae9eb85d9231e79c089c3a71d0a
m.piran <piran_mahsa@yahoo.com>
2021-08-07 10:21:16 +0430

    Functional components

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+function Card(){
+    return(
+        <div className="card">
+            <header className="header">
+                <h2>Header</h2>
+            </header>
+
+            <section className="card-body">
+                <p>Loremmmmmmmmmmmmmmm</p>
+            </section>
+
+            <footer className="footer">
+                <p>Footer</p>
+            </footer>
+        </div>
+    )
+}
+export default Card

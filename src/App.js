055c150ecb0bf7b3c48d4185c7091c52673ca769
m.piran <piran_mahsa@yahoo.com>
2021-08-07 11:15:18 +0430

    States

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import Card from "./Card";
 import Card2 from "./Card";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Card2 title="title1" body="this is 1"/>
-      <Card2 title="title2" body="this is 2"/>
-      <Card2 title="title3" body="this is 3"/>
-    </div>
-  );
+class App extends React.Component{
+    state={articles:[
+            {title:'title1' , body:'this is title1'},
+            {title:'title2' , body:'this is title2'}
+        ]}
+
+    constructor() {
+        super();
+        setTimeout(()=>
+            {
+                this.setState({articles:[
+                        {title:'title3' , body:'this is title3'},
+                        {title:'title4' , body:'this is title4'}
+                    ]})
+            }
+        ,2000)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Card2 title={this.state.articles[0].title} body={this.state.articles[0].body}/>
+                <Card2 title={this.state.articles[1].title} body={this.state.articles[1].body}/>
+            </div>
+        );
+    }
 }
 
 export default App;

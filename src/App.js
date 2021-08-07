f729fc5d347c97d17a2c6a3bc5ea0a87e9865549
m.piran <piran_mahsa@yahoo.com>
2021-08-07 10:44:13 +0430

    Class-based component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from "./Card";
+import Card2 from "./Card";
 
 function App() {
   return (
     <div className="App">
-      <Card title="title1" body="this is 1"/>
-      <Card title="title2" body="this is 2"/>
-      <Card title="title3" body="this is 3"/>
+      <Card2 title="title1" body="this is 1"/>
+      <Card2 title="title2" body="this is 2"/>
+      <Card2 title="title3" body="this is 3"/>
     </div>
   );
 }
---
+++ src/Card2.js
@@ -0,0 +1,26 @@
+import React from "react";
+// import React, component from "react";
+class Card2 extends React.Component{
+// class Card2 extends Component{
+    render() {
+        // let {title,body}=this.props
+        return(
+            <div className="card">
+                <header className="header">
+                    <h2>{this.title}</h2>
+                    {/*<h2>{title}</h2>*/}
+                </header>
+
+                <section className="card-body">
+                    <p>{this.body}</p>
+                    {/*<p>{body}</p>*/}
+                </section>
+
+                <footer className="footer">
+                    <p>Footer</p>
+                </footer>
+            </div>
+        )
+    }
+}
+export default Card2

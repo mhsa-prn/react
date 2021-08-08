bf1a43904b53bf751011b1f62b127d72479ac459
m.piran <piran_mahsa@yahoo.com>
2021-08-08 11:30:27 +0430

    Loading state

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Card2 from "./Card";
 import React from "react";
 
 class App extends React.Component{
-    state={articles:[
-            {title:'title1' , body:'this is title1'},
-            {title:'title2' , body:'this is title2'}
-        ]}
+    state = {
+        articles: [
+            {title: 'title1', body: 'this is title1'},
+            {title: 'title2', body: 'this is title2'}
+        ]
+    }
 
     // constructor() {
     //     super();
@@ -22,33 +24,45 @@ class App extends React.Component{
     //     ,2000)
     // }
 
-    loadMore = ()=>{
+    loadMore = () => {
+        this.setState({loading: true})
         let articles = [
-                {title:'title3', body:'this is title3'},
-                {title:'title4', body:'this is title4'}
-            ]
+            {title: 'title3', body: 'this is title3'},
+            {title: 'title4', body: 'this is title4'}
+        ]
 
-       this.setState(prevState=>{
-           return {
-               articles : [...prevState.articles , ...articles]
-           }
-       })
-
+        setTimeout(() => {
+                this.setState(prevState => {
+                    return {
+                        articles: [...prevState.articles, ...articles],
+                        loading: false
+                    }
+                })
+                this.setState({loading: false})
+            }
+            , 1000)
     }
 
 
     render() {
+        let articles = this.state.articles.map((article, index) => {
+            return <Card key={index} title={article.title} body={article.body}/>
+        })
         return (
             <div className="App">
-                {this.state.articles.map((article,index) => {
-                 return   <Card key={index} title={article.title} body={article.body}/>
-                })}
+                { articles }
+                {
+                    this.state.loading
+                        ? <div>Loding...</div>
+                        : null
+                }
                 {/*<Card2 title={this.state.articles[0].title} body={this.state.articles[0].body}/>*/}
                 {/*<Card2 title={this.state.articles[1].title} body={this.state.articles[1].body}/>*/}
                 <button onClick={this.loadMore}>Load More</button>
             </div>
         );
     }
+
 }
 
 export default App;

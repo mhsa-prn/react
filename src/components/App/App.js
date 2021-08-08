150baac77097ea65c7e4b931870a82d6e07b4383
m.piran <piran_mahsa@yahoo.com>
2021-08-08 14:27:20 +0430

    Event handlers

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -61,7 +61,10 @@ class App extends React.Component{
                 }
                 {/*<Card2 title={this.state.articles[0].title} body={this.state.articles[0].body}/>*/}
                 {/*<Card2 title={this.state.articles[1].title} body={this.state.articles[1].body}/>*/}
-                <button style={btnStyle} onClick={this.loadMore}>Load More</button>
+                <button style={btnStyle}
+                        onClick={this.loadMore}
+                        onMouseEnter={(e) => console.log('Enter')}
+                    >Load More</button>
             </div>
         );
     }
